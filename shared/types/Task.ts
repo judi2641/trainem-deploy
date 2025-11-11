3a3944d1871bdcdc3ae9662ec7a4028f23febe7c
judi2641 <judi2641@bht-berlin.de>
2025-11-11 13:14:40 +0100

    add new type

--- shared/types/Task.ts
+++ shared/types/Task.ts
@@ -1,7 +1,13 @@
-export interface Task {
+export interface TaskInDatabase {
     name: string,
     day: "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday",
     user_id: string,
     _id: string, // mongoose property
     __v: number, // mongoose property
+}
+
+export interface TaskToCreate {
+    name: string,
+    day: "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday",
+    user_id: string, 
 }
