54ce7afd30fd2db6f7b5741abed5ec7840e531ed
judi2641 <judi2641@bht-berlin.de>
2025-11-12 15:32:03 +0100

    add protected route for dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
 import Tasks from "./pages/Tasks";
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const ProtectedRoute = ({children}: {children: React.ReactNode}) => {
+  const  { isAuthenticated, isLoading } = useAuth0();
+
+  if(!isAuthenticated && !isLoading) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <>{children}</>;
+}
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/tasks" element={<Tasks />} />
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute>
+              <Dashboard/>
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/tasks" 
+          element={
+            <ProtectedRoute>
+              <Tasks/>
+            </ProtectedRoute>
+          } 
+        />
       </Routes>
     </Router>
   );
