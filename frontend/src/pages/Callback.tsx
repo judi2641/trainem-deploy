ea9a6484b338b18a3f215c4cd995dc32577fe683
judi2641 <judi2641@bht-berlin.de>
2025-12-11 17:36:17 +0100

    centered Loading.. after login

--- frontend/src/pages/Callback.tsx
+++ frontend/src/pages/Callback.tsx
@@ -1,52 +1,62 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Callback() {
-  const { user, isLoading } = useAuth0();
-  const navigate = useNavigate();
+	const { user, isLoading } = useAuth0();
+	const navigate = useNavigate();
 
+	useEffect(() => {
+		async function getExistingTrainingsplan() {
+			if (!isLoading && user) {
+				if (user.sub) {
+					const res_user = await fetch(
+						`http://localhost:3000/api/user/${encodeURIComponent(user.sub)}`,
+					);
+					if (!res_user.ok) {
+						const res_newuser = await fetch(`http://localhost:3000/api/user`, {
+							method: 'POST',
+							headers: {
+								'Content-Type': 'application/json',
+							},
+							body: JSON.stringify({ auth0id: user.sub, email: user.email }),
+						});
 
-  useEffect(() => {
+						if (!res_newuser.ok) {
+							console.log('fehler beim erstellen');
+							navigate('/');
+							return;
+						}
+					}
 
-    async function getExistingTrainingsplan(){
-        if (!isLoading && user) {
-          if(user.sub){
+					const res_trainingsplan = await fetch(
+						`http://localhost:3000/api/trainingsplan/${encodeURIComponent(user.sub)}`,
+					);
+					if (!res_trainingsplan.ok) {
+						navigate('/onboarding');
+					} else {
+						navigate('/dashboard');
+					}
+				} else {
+					console.log('keine user.sub');
+					navigate('/');
+				}
+			}
+		}
+		getExistingTrainingsplan();
+	}, [isLoading, user]);
 
-            const res_user = await fetch(`http://localhost:3000/api/user/${encodeURIComponent(user.sub)}`);
-            if(!res_user.ok){
-              
-              const res_newuser = await fetch(`http://localhost:3000/api/user`, {
-                method: "POST",
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ "auth0id": user.sub, "email": user.email })
-              });
-              
-              if(!res_newuser.ok){
-                console.log("fehler beim erstellen");
-                navigate("/");
-                return;
-              }                  
-            }
-            
-            const res_trainingsplan = await fetch(`http://localhost:3000/api/trainingsplan/${encodeURIComponent(user.sub)}`);
-            if(!res_trainingsplan.ok) {
-              navigate("/onboarding");
-            }
-            else{
-              navigate("/dashboard");
-            }
-          }
-          else{
-            console.log("keine user.sub");
-            navigate("/");
-          }
-        }
-    }
-    getExistingTrainingsplan();
-  }, [isLoading, user]);
-
-  return <div>Loading…</div>;
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				height: '100vh',
+				width: '100%',
+			}}
+		>
+			Loading…
+		</div>
+	);
 }
